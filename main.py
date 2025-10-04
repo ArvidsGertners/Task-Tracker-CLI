b137764761636f11a22ba2082d565e453001be07
Arvids Gertners <Arvids.grt@gmail.com>
2025-10-04 18:14:55 +0200

    Marking tasks as done, in-progress and todo, added.

--- main.py
+++ main.py
@@ -22,14 +22,26 @@ deleteParser.add_argument("id", help="The task ID to delete")
 listParser = subParsers.add_parser("list", help="List all tasks")
 listParser.add_argument("type", help="The type of task to list", choices=['todo', 'in-progress', 'done'], nargs='?')
 
+markInProgress = subParsers.add_parser("mark-in-progress", help="Mark a task in progress")
+markInProgress.add_argument("id", help="The task ID to mark in progress")
+
+markDone = subParsers.add_parser("mark-done", help="Mark a task in done")
+markDone.add_argument("id", help="The task ID to mark in done")
+
+markToDo = subParsers.add_parser("mark-todo", help="Mark a task to be done")
+markToDo.add_argument("id", help="The task ID to mark todo")
+
 args = parser.parse_args()
 
 
 def load_data():
-    if not os.path.exists(TASKS_PATH):
+    try:
+        with open(TASKS_PATH, 'r') as f:
+            return json.load(f)
+    except json.decoder.JSONDecodeError:
         return {"tasks": []}
-    with open(TASKS_PATH, 'r') as f:
-        return json.load(f)
+    except FileNotFoundError:
+        return {"tasks": []}
 
 
 def save_data(data):
@@ -75,14 +87,26 @@ def add():
 
 def update():
     data = load_data()
-    data["tasks"][get_task_index(data["tasks"], args.id)]["description"] = args.newDescription
+    task_index = get_task_index(data["tasks"], args.id)
+    if task_index is None:
+        print(f"Task with ID {args.id} not found!")
+        return
+
+    data["tasks"][task_index]["description"] = args.newDescription
     save_data(data)
+    print(f"Updated task {args.id}")
 
 
 def delete():
     data = load_data()
-    data["tasks"].pop(get_task_index(data["tasks"], args.id))
+    task_index = get_task_index(data["tasks"], args.id)
+    if task_index is None:
+        print(f"Task with ID {args.id} not found!")
+        return
+
+    data["tasks"].pop(task_index)
     save_data(data)
+    print(f"Deleted task {args.id}")
 
 
 def list_tasks():
@@ -101,6 +125,37 @@ def list_tasks():
             print(f"ID: {t["id"]} - {t['description']}")
 
 
+def mark_in_progress():
+    data = load_data()
+    task_index = get_task_index(data["tasks"], args.id)
+    if task_index is None:
+        print(f"Task with ID {args.id} not found!")
+        return
+
+    data["tasks"][task_index]["status"] = 'in-progress'
+    save_data(data)
+
+
+def mark_done():
+    data = load_data()
+    task_index = get_task_index(data["tasks"], args.id)
+    if task_index is None:
+        print(f"Task with ID {args.id} not found!")
+        return
+    data["tasks"][task_index]["status"] = 'done'
+    save_data(data)
+
+
+def mark_to_do():
+    data = load_data()
+    task_index = get_task_index(data["tasks"], args.id)
+    if task_index is None:
+        print(f"Task with ID {args.id} not found!")
+        return
+    data["tasks"][task_index]["status"] = 'todo'
+    save_data(data)
+
+
 if args.command == "add":
     add()
 elif args.command == "update":
@@ -109,3 +164,9 @@ elif args.command == "delete":
     delete()
 elif args.command == "list":
     list_tasks()
+elif args.command == "mark-in-progress":
+    mark_in_progress()
+elif args.command == "mark-done":
+    mark_done()
+elif args.command == "mark-todo":
+    mark_to_do()
